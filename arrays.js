c8036bc9ac292c8004e09b7b3df9a363c0bfbb5c
rotembm12 <46103618+rotembm12@users.noreply.github.com>
2024-12-08 11:48:05 +0200

    done

---
+++ arrays.js
@@ -0,0 +1,45 @@
+// const array = [
+//   "rotem",
+//   32,
+//   function x() {
+//     console.log("im a function");
+//   },
+//   "asdasda",
+// ];
+// // console.log(array);
+// // array.push("im new here");
+// // console.log(array);
+// // console.log(array.shift());
+// // console.log(array);
+// // array.pop();
+// // console.log(array);
+// // array.unshift("i pushed the line");
+// console.log(array);
+
+// function addElementToArray(arr) {
+//   arr.push("element");
+//   let x = 10;
+// }
+
+// addElementToArray(array);
+
+// console.log(array);
+
+// for (let val of array) {
+//   console.log(val);
+// }
+
+// for (let key in array) {
+//   console.log(key);
+// }
+// for (let i = 0; i < array.length; i++) {
+//   console.log(array[i]);
+// }
+
+// ///
+// const styles = ["JAzz", "dance", "...", "...", "..."];
+// styles.push("rock n roll");
+
+// styles.shift();
+// styles.unshift("rap", "asdfasd", "asfas");
+document.body.style 
---
+++ object-comparisons.js
@@ -0,0 +1,12 @@
+if (10 === 10) {
+}
+
+if ("sad" === "sad") {
+}
+
+let user1 = { name: "rotem" };
+let user2 = { name: "rotem" };
+
+function isTheSameUser(userX, userY) {
+  return userX.name === userY.name && userX.age === userY.age;
+}
